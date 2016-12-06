f4d08d92f4541a0dc20eaf90a0167024a9263538
mengwei <meng.wei@zte.com.cn>
2016-12-07 07:58:44 +0800

    test5

--- ConsoleApplication1/ConsoleApplication1/draw.cpp
+++
@@ -1,8 +0,0 @@
-#include"includes.h"
-
-void draw(int n)
-{
-		drawtuxing(n);
-	
-}
-
--- ConsoleApplication3/ConsoleApplication3/head.h
+++
@@ -1,8 +0,0 @@
-//
-//#define Big(a,b) (((a)>=(b))?(a):(b))
-//#include <iostream>
-//using namespace std;
-//
-//int Small(int a,int b);
-//int small_all(int a,int b);
-//
